94957d2c4ce83331ce56c97ffc3bd08ffb7c299d
Tom321g <studencik124@gmail.com>
2017-04-01 15:37:33 +0200

    Update kalkulator.java

--- kalkulator.java
+++ kalkulator.java
@@ -1,1 +1,35 @@
-#
+package javaKalkulator;
+
+/**
+ *
+ * @author Tomek
+ */
+public class MatKalkulator extends javax.swing.JFrame {
+
+    /**
+     * Creates new form MatKalkulator
+     */
+    public MatKalkulator() {
+        initComponents();
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
+    private void initComponents() {
+
+        txtWynik = new javax.swing.JTextField();
+        jPanel2 = new javax.swing.JPanel();
+        btnJeden = new javax.swing.JButton();
+        btnDwa = new javax.swing.JButton();
+        btnTrzy = new javax.swing.JButton();
+        btnCztery = new javax.swing.JButton();
+        btnPiec = new javax.swing.JButton();
+        btnSzesc = new javax.swing.JButton();
+        btnDziewiec = new javax.swing.JButton();
+        btnOsiem = new javax.swing.JButton();
+        btnSiedem = new javax.swing.JButton();
+        btnBin = new javax.swing.JButton();
+        btnOct = new javax.swing.JButton();
+        btnHex = new javax.swing.JButton();
+    }
