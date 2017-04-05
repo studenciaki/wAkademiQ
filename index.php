79de5473777e750671ac72f9e1e9503035e0492d
darbro96 <dbronikowsk@gmail.com>
2017-04-05 20:21:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -13,14 +13,25 @@ session_start();
     Nr albumu:  <input type="text" name="nr_albumu"/>
     <input type="submit" name="submit" value="WERYFIKUJ"/>
 </form>
+<font size="5" face="Consolas">
+<script type="text/javascript">
+var data = new Date();
+var godz = data.getHours();
+var min = data.getMinutes();
+if(min<10)
+	min='0'+min;
+document.write(godz + ':' + min);
+</script>
+</font>
 <?php
-if(isset($_SESSION['nr_albumu']))
+if(isset($_SESSION['nr_albumu']) && isset($_SESSION['powiadomiony']))
 {
 	echo '<script type="text/javascript">';
     echo 'alert("Mieszkaniec poinformowany!");';
     echo '</script>';
 }
 unset($_SESSION['nr_albumu']);
+unset($_SESSION['powiadomiony']);
 ?>
 </body>
 </html>
