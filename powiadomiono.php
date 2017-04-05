183694c9fa8c4ede03e72acb9c7cb83c82428f08
darbro96 <dbronikowsk@gmail.com>
2017-04-05 20:20:20 +0200

    Update powiadomiono.php

--- powiadomiono.php
+++ powiadomiono.php
@@ -1,20 +1,10 @@
 <?php
 session_start();
-?>
-<html>
-<head>
-<title>System Uwierzytelniania Mieszkańca</title>
-<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-<center>
-<?php 
 $nr_albumu=$_SESSION['nr_albumu'];
 $polaczenie=@mysql_connect('localhost','root','');
 $baza = @mysql_select_db('wakademiq', $polaczenie);
 $wynik = mysql_query("UPDATE mieszkancy SET wiadomosci_administracja=NULL WHERE nr_albumu=$nr_albumu;") or die('Błąd zapytania!');
+$_SESSION['powiadomiony']=true;
 header('Location: \wakademiq/sum/index.php');
 mysql_close($polaczenie);
 ?>
-</body>
-</html>
