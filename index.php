e2fe70282d691c742d66da98629ba5a8a9ae7a10
darbro96 <dbronikowsk@gmail.com>
2017-04-08 14:41:59 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,8 +10,8 @@ session_start();
 <h1><sub>w</sub>AkademiQ</h1>
 <h3>System Uwierzytelniania Mieszkańca</h3>
 <form action="weryfikacja.php" method="post">
-    Nr albumu:  <input type="text" name="nr_albumu"/>
-    <input type="submit" name="submit" value="WERYFIKUJ"/>
+Nr albumu:  <input type="text" name="nr_albumu"/>
+<input type="submit" name="submit" value="WERYFIKUJ"/>
 </form>
 <font size="5" face="Consolas">
 <script type="text/javascript">
@@ -29,21 +29,28 @@ if(isset($_SESSION['nr_albumu']) && isset($_SESSION['powiadomiony']))
 	echo '<script type="text/javascript">';
     echo 'alert("Mieszkaniec poinformowany!");';
     echo '</script>';
+	unset($_SESSION['nr_albumu']);
+	unset($_SESSION['powiadomiony']);
 }
 if(isset($_SESSION['blad_zapytania']))
 {
 	echo '<script type="text/javascript">';
     echo 'alert("Błąd zapytania!");';
     echo '</script>';
+	unset($_SESSION['blad_zapytania']);
 }
-unset($_SESSION['nr_albumu']);
-unset($_SESSION['powiadomiony']);
-unset($_SESSION['blad_zapytania']);
 if(isset($_SESSION['blad']))
 {
 	echo("</font><br><br><font color='red'>Błąd odczytu numeru albumu!");
+	unset($_SESSION['blad']);
 }
-unset($_SESSION['blad']);
+if(isset($_SESSION['brak_w_bazie']))
+{
+	echo '<script type="text/javascript">';
+    echo 'alert("Brak numeru albumu w bazie!");';
+    echo '</script>';
+	unset($_SESSION['brak_w_bazie']);
+}
 ?>
 </body>
 </html>
