9bdb42523bef32f22ad326df035f054f8cb72d57
darbro96 <dbronikowsk@gmail.com>
2017-04-10 21:20:25 +0200

    Update weryfikacja.php

--- weryfikacja.php
+++ weryfikacja.php
@@ -4,11 +4,11 @@ session_start();
 <html>
 <head>
 <title>System Uwierzytelniania Mieszkańca</title>
-<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" href="mieszkancy.css" type="text/css">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
 <body>
 <center>
-<a href='index.php'>Powrót</a><br>
 <?php
    $nr_albumu=$_POST['nr_albumu'];
    if($nr_albumu=='')
@@ -23,46 +23,30 @@ session_start();
    $wynik = mysql_query("SELECT * FROM mieszkancy WHERE nr_albumu=$nr_albumu;") or die(header('Location: \wakademiq/sum/index.php') && $_SESSION['blad_zapytania']=true);
    
    if(mysql_num_rows($wynik) > 0) {
-	   echo ("Mieszkaniec: <br>");
+	   echo ('<div id="panel_glowny"><div id="panel_mieszkanca">');
     while($d = mysql_fetch_assoc($wynik)) {
-		echo ("Imię: <b>");
+		echo ("<div id='zdjecie'><img src=\"zdjecia/$nr_albumu.png\" width=\"250px\" height=\"300px\"></div>");
+		echo ("<div id=\"imie_nazwisko\">");
 		echo $d['imie'];
-		echo ("<br></b>Nazwisko: <b>");
+		echo ("&nbsp;");
 		echo $d['nazwisko'];
-		echo ("<br></b>Nr pokoju: <b>");
-		echo $d['nr_pokoju'];
-		echo ("<br></b>Wydział: <b>");
-		echo $d['wydzial'];
-		if($d['wiadomosci_administracja']!='')
-		{
-			echo "</b><br><body bgcolor='red'>Wiadomość od administracji: <b>";
-			echo $d['wiadomosci_administracja'];
-			echo "<form action=\"powiadomiono.php\ method=\"post\">";
-			echo "<input type=\"submit\" name=\"submit\" value=\"POWIADOM\"/></form>";
-			$_SESSION['nr_albumu']=$d['nr_albumu'];
-		}
-		echo ("<br><br></b><div id='ramka'><img src=\"zdjecia/$nr_albumu.png\"></div>");
+		echo ("</div></div><div id=\"pasek\"></div>");
 		$lokator=$d['lokator'];
 		$wynik2 = mysql_query("SELECT * FROM mieszkancy WHERE nr_albumu=$lokator;") or die('');
 		if(mysql_num_rows($wynik2) > 0) {
-	   echo ("<br><br>Lokator: <br>");
+	   echo ("<div id=\"panel_mieszkanca\">");
     while($e = mysql_fetch_assoc($wynik2)) {
-		echo ("Imię: <b>");
+		echo ("<div id='zdjecie'><img src=\"zdjecia/$lokator.png\" width=\"250px\" height=\"300px\"></div>");
+		echo ("<div id=\"imie_nazwisko\">");
 		echo $e['imie'];
-		echo ("<br></b>Nazwisko: <b>");
+		echo ("&nbsp;");
 		echo $e['nazwisko'];
-		echo ("<br></b>Nr pokoju: <b>");
-		echo $e['nr_pokoju'];
-		echo ("<br></b>Wydział: <b>");
-		echo $e['wydzial'];
-		if($e['wiadomosci_administracja']!='')
-		{
-			echo "</b><br><body bgcolor='red'>Wiadomość od administracji: <b>";
-			echo $e['wiadomosci_administracja'];
-		}
-		echo ("<br><br></b><div id='ramka'><img src=\"zdjecia/$lokator.png\"></div>");
+		echo ("</div></div>");
 	}
     }
+	echo ("<div id=\"numer_pokoju\">");
+		echo $d['nr_pokoju'];
+		echo("</div></div>");
 }
    }
    else
