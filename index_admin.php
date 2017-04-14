6a668391f0beb792161f00d7e7a4294ceb4ad220
darbro96 <dbronikowsk@gmail.com>
2017-04-14 10:53:27 +0200

    Add files via upload

---
+++ index_admin.php
@@ -0,0 +1,21 @@
+﻿<html>
+<head><title>wAkademiQ</title>
+<link rel="stylesheet" href="admin.css" type="text/css">
+<link href='http://fonts.googleapis.com/css?family=Lato:400,900&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+
+<body>
+<div id="strona">
+ <div id="logo">Panel administracyjny</div>
+ <div id="panel">
+
+	<a href="formularz_dodawania_mieszkanca.php"><div id="przycisk">Dodaj</div></a>
+	<a href="#"><div id="przycisk2">Edytuj</div></a>
+	<a href="#"><div id="przycisk3">Wymelduj</div></a>
+	<a href="#"><div id="przycisk4">Powiadom</div></a>
+  </div>
+
+</div>
+</body>
+</html>
