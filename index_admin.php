395a82ccd6f6cccb8ccc7a2c49f0fd316df12cce
darbro96 <dbronikowsk@gmail.com>
2017-04-15 14:28:06 +0200

    Update index_admin.php

--- index_admin.php
+++ index_admin.php
@@ -1,4 +1,4 @@
-﻿<html>
+<html>
 <head><title>wAkademiQ</title>
 <link rel="stylesheet" href="admin.css" type="text/css">
 <link href='http://fonts.googleapis.com/css?family=Lato:400,900&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
@@ -11,7 +11,7 @@
  <div id="panel">
 
 	<a href="formularz_dodawania_mieszkanca.php"><div id="przycisk">Dodaj</div></a>
-	<a href="#"><div id="przycisk2">Edytuj</div></a>
+	<a href="formularz_edycji_mieszkanca.php"><div id="przycisk2">Edytuj</div></a>
 	<a href="#"><div id="przycisk3">Wymelduj</div></a>
 	<a href="#"><div id="przycisk4">Powiadom</div></a>
   </div>
