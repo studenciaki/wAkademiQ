9f7341a9ff0525668dda85ba726755c669006dee
darbro96 <dbronikowsk@gmail.com>
2017-04-19 20:14:51 +0200

    Update edycja_mieszkanca.php

--- edycja_mieszkanca.php
+++ edycja_mieszkanca.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 session_start();
 ?>
 <html>
@@ -14,10 +14,6 @@ $nazwisko=$_POST['nazwisko'];
 $nr_albumu=$_POST['nr_albumu'];
 $wydzial=$_POST['wydzial'];
 $nr_pokoju=$_POST['nr_pokoju'];
-$lokator=$_POST['lokator'];
-
-if($lokator=='brak')
-	$lokator='';
 
 echo ('<form action="edycja_mieszkanca_w_bazie.php" method="POST"><br>Imię:');
 echo ("<input type=\"text\" name=\"imie\" value=\"$imie\">");
@@ -25,11 +21,7 @@ echo ('<br>Nazwisko:');
 echo ("<input type=\"text\" name=\"nazwisko\" value=\"$nazwisko\">");
 echo ('<br>Nr albumu:');
 echo ("<input type=\"text\" name=\"nr_albumu\" value=\"$nr_albumu\" readonly>");
-echo ('<br>Wydział:');
-echo ("<input type=\"text\" name=\"wydzial\" value=\"$wydzial\">");
 echo ('<br>Nr pokoju:');
 echo ("<input type=\"text\" name=\"nr_pokoju\" value=\"$nr_pokoju\">");
-echo ('<br>Lokator:');
-echo ("<input type=\"text\" name=\"lokator\" value=\"$lokator\">");
 echo ("<br><input type=\"submit\" value=\"ZAPISZ W BAZIE\">");
 ?>
