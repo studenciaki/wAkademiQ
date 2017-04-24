93d69eef4b9b02591f66ba5016e5c6e473838d97
darbro96 <dbronikowsk@gmail.com>
2017-04-24 23:18:10 +0200

    Update edycja_mieszkanca.php

--- edycja_mieszkanca.php
+++ edycja_mieszkanca.php
@@ -15,6 +15,23 @@ $nr_albumu=$_POST['nr_albumu'];
 $wydzial=$_POST['wydzial'];
 $nr_pokoju=$_POST['nr_pokoju'];
 
+if(isset($_SESSION['edycja_imie']))
+{
+	$imie=$_SESSION['edycja_imie'];
+	unset($_SESSION['edycja_imie']);
+}
+if(isset($_SESSION['edycja_nazwisko']))
+{
+	$nazwisko=$_SESSION['edycja_nazwisko'];
+	unset($_SESSION['edycja_nazwisko']);
+}
+	
+if(isset($_SESSION['edycja_nr_albumu']))
+{
+	$nr_albumu=$_SESSION['edycja_nr_albumu'];
+	unset($_SESSION['edycja_nr_albumu']);
+}
+
 echo ('<form action="edycja_mieszkanca_w_bazie.php" method="POST"><br>Imię:');
 echo ("<input type=\"text\" name=\"imie\" value=\"$imie\">");
 echo ('<br>Nazwisko:');
@@ -24,4 +41,11 @@ echo ("<input type=\"text\" name=\"nr_albumu\" value=\"$nr_albumu\" readonly>");
 echo ('<br>Nr pokoju:');
 echo ("<input type=\"text\" name=\"nr_pokoju\" value=\"$nr_pokoju\">");
 echo ("<br><input type=\"submit\" value=\"ZAPISZ W BAZIE\">");
+if(isset($_SESSION['pokoj_przepelniony']))
+{
+	echo '<script type="text/javascript">';
+    echo 'alert("Wybrany pokój jest zajęty!");';
+    echo '</script>';
+	unset($_SESSION['pokoj_przepelniony']);
+}
 ?>
