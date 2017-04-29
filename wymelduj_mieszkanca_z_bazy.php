a7f8863398ef316c1ca070fa068d3ad2e42d80a9
darbro96 <dbronikowsk@gmail.com>
2017-04-29 17:21:06 +0200

    Update wymelduj_mieszkanca_z_bazy.php

--- wymelduj_mieszkanca_z_bazy.php
+++ wymelduj_mieszkanca_z_bazy.php
@@ -1,10 +1,9 @@
-﻿<?php
+<?php
 session_start();
 
 $imie=$_POST['imie'];
 $nazwisko=$_POST['nazwisko'];
 $nr_albumu=$_POST['nr_albumu'];
-$wydzial=$_POST['wydzial'];
 $nr_pokoju=$_POST['nr_pokoju'];
 $lokator=$_POST['lokator'];
 
@@ -12,12 +11,12 @@ $imie=htmlentities($imie, ENT_QUOTES, "UTF-8");
 $nazwisko=htmlentities($nazwisko, ENT_QUOTES, "UTF-8");
 $nr_albumu=htmlentities($nr_albumu, ENT_QUOTES, "UTF-8");
 $nr_pokoju=htmlentities($nr_pokoju, ENT_QUOTES, "UTF-8");
-$wydzial=htmlentities($wydzial, ENT_QUOTES, "UTF-8");
 $lokator=htmlentities($lokator, ENT_QUOTES, "UTF-8");
 
 $polaczenie=@mysql_connect('localhost','root','');
 $baza = @mysql_select_db('wakademiq', $polaczenie);
 $wymelduj=mysql_query("DELETE FROM mieszkancy WHERE nr_albumu=$nr_albumu;") or die ("Błąd zapytania imie");
+$_SESSION['sukces_wymeldowano']=true;
 mysql_close($polaczenie);
 header('Location: \wakademiq/panel_administracji/');
 ?>
