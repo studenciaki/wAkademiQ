9692fb4d80b1a7ea7f9090292b15e1a975dfd2b1
darbro96 <dbronikowsk@gmail.com>
2017-04-29 17:19:05 +0200

    Update edycja_mieszkanca_w_bazie.php

--- edycja_mieszkanca_w_bazie.php
+++ edycja_mieszkanca_w_bazie.php
@@ -48,10 +48,12 @@ if($czy_ok==true)
 	if(mysql_num_rows($info)>0)
 			{
 				while($d=mysql_fetch_assoc($info))
+				{
 					$stare_imie=$d['imie'];
 					$stare_nazwisko=$d['nazwisko'];
 					$lokator=$d['lokator'];
 					$stary_pokoj=$d['nr_pokoju'];
+				}
 			}
 	if($stare_imie!=$imie)
 		$edytuj_imie=mysql_query("UPDATE mieszkancy SET imie='$imie' WHERE nr_albumu=$nr_albumu;") or die ("Błąd zapytania imie");
@@ -62,12 +64,11 @@ if($czy_ok==true)
 		$spr_pokoj=mysql_query("SELECT * FROM mieszkancy WHERE nr_pokoju='$nr_pokoju';");
 		if(mysql_num_rows($spr_pokoj)>1)
 		{
-			while($d=mysql_fetch_assoc($info))
 			$_SESSION['edycja_imie']=$imie;
 			$_SESSION['edycja_nazwisko']=$nazwisko;
 			$_SESSION['edycja_nr_albumu']=$nr_albumu;
 			$_SESSION['pokoj_przepelniony']=true;
-			header('Location: \wakademiq/panel_administracji/edycja_mieszkanca.php');	
+			header('Location: \wakademiq/panel_administracji/edycja_mieszkanca.php');
 		}
 		else if(mysql_num_rows($spr_pokoj)==1)
 		{
@@ -77,6 +78,7 @@ if($czy_ok==true)
 				while($d=mysql_fetch_assoc($jaki_nowy_lokator))
 					$nowy_lokator=$d['nr_albumu'];
 			}
+			if(strlen($lokator)>0)
 			$usun_u_starego_lokatora=mysql_query("UPDATE mieszkancy SET lokator=NULL WHERE nr_albumu=$lokator;") or die ("Nie usunięto u starego lokatora");
 			$ustaw_pokoj=mysql_query("UPDATE mieszkancy SET nr_pokoju='$nr_pokoju' WHERE nr_albumu=$nr_albumu;");
 			$dodaj_nowego_lokatora=mysql_query("UPDATE mieszkancy SET lokator=$nowy_lokator WHERE nr_albumu=$nr_albumu;");
