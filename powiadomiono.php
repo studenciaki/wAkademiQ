ce8eddc9bc212ffc27436ecf8ac06d440991df41
darbro96 <dbronikowsk@gmail.com>
2017-05-15 20:06:22 +0200

    Update powiadomiono.php

--- powiadomiono.php
+++ powiadomiono.php
@@ -5,11 +5,14 @@ if(!isset($_SESSION['nr_albumu']))
 	header('Location: \wakademiq/sum/index.php');
 	exit();
 }
-$nr_albumu=$_SESSION['nr_albumu'];
-$polaczenie=@mysql_connect('localhost','root','');
-$baza = @mysql_select_db('wakademiq', $polaczenie);
-$wynik = mysql_query("UPDATE mieszkancy SET wiadomosci_administracja=NULL WHERE nr_albumu=$nr_albumu;") or die('Błąd zapytania!');
-$_SESSION['powiadomiony']=true;
-header('Location: \wakademiq/sum/index.php');
-mysql_close($polaczenie);
+else
+{
+	$nr_albumu=$_SESSION['nr_albumu'];
+	$polaczenie=@mysql_connect('localhost','root','');
+	$baza = @mysql_select_db('wakademiq', $polaczenie);
+	$wynik = mysql_query("UPDATE mieszkancy SET wiadomosci_administracja=NULL WHERE nr_albumu=$nr_albumu;") or die('Błąd zapytania!');
+	$_SESSION['powiadomiony']=true;
+	mysql_close($polaczenie);	
+	header('Location: \wakademiq/sum/index.php');
+}
 ?>
