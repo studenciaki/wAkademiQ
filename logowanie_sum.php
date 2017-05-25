0c0f6f7f74d83eded4a1826eba637c6c1da02e7f
darbro96 <dbronikowsk@gmail.com>
2017-05-25 17:13:22 +0200

    Add files via upload

---
+++ logowanie_sum.php
@@ -0,0 +1,13 @@
+﻿<?php
+session_start();
+$pin=$_POST['pin'];
+if($pin!=5421)
+{
+	header('Location: \wakademiq/index.html');
+}
+else
+{
+	$_SESSION['weryfikacja_dostep']=true;
+	header('Location: \wakademiq/sum/index.php');
+}
+?>
