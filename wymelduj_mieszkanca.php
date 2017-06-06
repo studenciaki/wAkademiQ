ff61d40fb8b93da514b6681f8616dbdbe0723fe8
darbro96 <dbronikowsk@gmail.com>
2017-06-06 17:28:08 +0200

    Update wymelduj_mieszkanca.php

--- wymelduj_mieszkanca.php
+++ wymelduj_mieszkanca.php
@@ -1,13 +1,41 @@
 <?php
 session_start();
+if(!isset($_SESSION['dostep_panel']))
+	header('Location: \wakademiq/panel_administracji/logowanie.php');
 ?>
 <html>
-<head><title>wAkademiQ - wymelduj mieszkańca</title>
-<link rel="stylesheet" href="admin.css" type="text/css">
+<head><title>AKADEMIQ</title>
+<link rel="stylesheet" href="panel.css" type="text/css">
 <link href='http://fonts.googleapis.com/css?family=Lato:400,900&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="Shortcut icon" href="img/favicon.ico" />
 </head>
-<body>
+
+<body link="white" vlink="white" alink="white">
+
+<div id="panel">
+	<div id="panel_gora">
+	<div id="pasek1"></div>
+	<a href="panel.php"><div id="pasek2"><img src="logo.png"></div></a>
+	<div id="pasek3"></div>
+	<div id="pasek2"><img src="awatar3.png"> <a href="wylogowanie.php"> <img src="wylogoj.png"></a></div>
+	<div id="pasek1"></div>
+	</div>
+	
+	<div id="panel_lewa">
+		<div id="menu">Mieszkańcy<div id="menu2">
+		<a href="dodaj.php"><div id="przycisk_menu">Dodaj</div></a>
+		<a href="edytuj.php"><div id="przycisk_menu">Edytuj</div></a>
+		<a href="wymelduj.php"><div id="przycisk_menu">Wymelduj</div></a>
+		<a href="powiadom.php"><div id="przycisk_menu">Powiadom</div></a>
+		</div></div>
+		<div id="menu">Panel<div id="menu2">
+		<a href="ustawienia.php"><div id="przycisk_menu">Ustawienia</div></a>
+		<a href="serwis.php"><div id="przycisk_menu">Kontakt z serwis</div></a>
+		</div></div>
+	</div>
+	
+	<div id="panel_prawa"><center>Wymelduj mieszkańca<bR><br> &nbsp
 <?php
 
 $_SESSION['form_imie']=$_POST['imie'];
@@ -102,3 +130,6 @@ else
 	}
 }
 ?>
+</div>
+</body>
+</html>
