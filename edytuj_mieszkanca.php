95eb5a1884970c084bb15843bd22d271a0a73ed4
darbro96 <dbronikowsk@gmail.com>
2017-06-06 17:21:41 +0200

    Update edytuj_mieszkanca.php

--- edytuj_mieszkanca.php
+++ edytuj_mieszkanca.php
@@ -1,23 +1,48 @@
 <?php
 session_start();
+if(!isset($_SESSION['dostep_panel']))
+	header('Location: \wakademiq/panel_administracji/logowanie.php');
 ?>
 <html>
-<head><title>wAkademiQ - edycja mieszkańca</title>
-<link rel="stylesheet" href="admin.css" type="text/css">
+<head><title>AKADEMIQ</title>
+<link rel="stylesheet" href="panel.css" type="text/css">
 <link href='http://fonts.googleapis.com/css?family=Lato:400,900&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="Shortcut icon" href="img/favicon.ico" />
 </head>
-<body>
-<?php
 
+<body link="white" vlink="white" alink="white">
+
+<div id="panel">
+	<div id="panel_gora">
+	<div id="pasek1"></div>
+	<a href="panel.php"><div id="pasek2"><img src="logo.png"></div></a>
+	<div id="pasek3"></div>
+	<div id="pasek2"><img src="awatar3.png"> <a href="wylogowanie.php"> <img src="wylogoj.png"></a></div>
+	<div id="pasek1"></div>
+	</div>
+	
+	<div id="panel_lewa">
+		<div id="menu">Mieszkańcy<div id="menu2">
+		<a href="dodaj.php"><div id="przycisk_menu">Dodaj</div></a>
+		<a href="edytuj.php"><div id="przycisk_menu">Edytuj</div></a>
+		<a href="wymelduj.php"><div id="przycisk_menu">Wymelduj</div></a>
+		<a href="powiadom.php"><div id="przycisk_menu">Powiadom</div></a>
+		</div></div>
+		<div id="menu">Panel<div id="menu2">
+		<a href="ustawienia.php"><div id="przycisk_menu">Ustawienia</div></a>
+		<a href="serwis.php"><div id="przycisk_menu">Kontakt z serwis</div></a>
+		</div></div>
+	</div>
+	
+	<div id="panel_prawa"><center>Panel główny<bR><br> &nbsp
+<?php
 $_SESSION['form_imie']=$_POST['imie'];
 $_SESSION['form_nazwisko']=$_POST['nazwisko'];
-
 if(strlen($_POST['imie'])>0 && strlen($_POST['nazwisko'])>0)
 {
 	$imie=$_POST['imie'];
 	$nazwisko=$_POST['nazwisko'];
-
 	$imie=htmlentities($imie, ENT_QUOTES, "UTF-8");
 	$nazwisko=htmlentities($nazwisko, ENT_QUOTES, "UTF-8");
 	
@@ -87,3 +112,6 @@ else
 	}
 }
 ?>
+</div>
+</body>
+</html>
